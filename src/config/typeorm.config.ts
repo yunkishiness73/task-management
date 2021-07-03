d1a748e72f2f98503ddb0b4a88f7c275f081f2b6
Kiet Nguyen <yunkishine.ss@gmail.com>
2021-07-04 01:01:46 +0700

    fix: change ssl postgres config file

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,6 +11,8 @@ export const config: TypeOrmModuleOptions = {
   entities: ["dist/**/*.entity{.ts,.js}"],
   synchronize: true,
   logging: true,
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   url: process.env.DATABASE_URL || 'postgres://itrzgigzqfahki:d6b2b7d54633921717618677575ad0b0ea23e5ece1df83afebf5657265073ca6@ec2-54-163-97-228.compute-1.amazonaws.com:5432/d2u3gne68rhljb',
 }
