c10e95f697a63606984df912d2a19625801e4208
Kiet Nguyen <yunkishine.ss@gmail.com>
2021-07-04 00:40:37 +0700

    Fix: port

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -12,5 +12,5 @@ export const config: TypeOrmModuleOptions = {
   synchronize: true,
   logging: true,
   ssl: true,
-  url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL || 'postgres://itrzgigzqfahki:d6b2b7d54633921717618677575ad0b0ea23e5ece1df83afebf5657265073ca6@ec2-54-163-97-228.compute-1.amazonaws.com:5432/d2u3gne68rhljb',
 }
